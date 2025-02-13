1859c9d11a35543882d344ae15f4d5e78d5f19e4
SonnieCodes <mgathoni.gathoni9@gmail.com>
2025-02-13 05:46:40 +0300

    anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,23 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self._word = word
+
+    @property
+    def word(self):
+        return self._word
+    
+    @word.setter
+    def word(self, value):
+        if isinstance(value, str):
+            self._word = value.lower()
+        else:
+            raise ValueError("Word must be a string")    
+        
+    def match(self, words):
+        word_list = []
+        for a in words:
+            if sorted(a.lower()) == sorted(self._word):
+                word_list.append(a)
+        return word_list
+            
